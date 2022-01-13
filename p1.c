c08c2c6cfbd3273a581bdcce7ea392723d9c2782
Rohan0708 <rohananil0708@gmail.com>
2022-01-13 15:51:46 +0530

    Update of eos exam question

--- p1.c
+++ p1.c
@@ -1,35 +1,46 @@
 /* Shared memory program
-   */
+ */
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <string.h>
+#define SIZE 20
+int i = 0;
 
-
-
-int *count,i,n;
-char str[100];
+int shmd;
+//char *data;
 
 int main()
 {
-    /* code */
-    int shmd;
-    shmd = shm_open("/dictionary.txt", O_RDWR , S_IRUSR | S_IWUSR);// it is of path name , flags ,mode(we used as user call f'n)
-    ftruncate(shmd,sizeof(int)); //file named by path & sizeof length
-    count = (int *)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);//virtual address space of the calling process IT HASaddress.length,prot,flags,fd,offset.
+     char string[SIZE];
+
+    shmd = shm_open("/cdacshm", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);                     // it is of path name , flags ,mode(we used as user call f'n)
+    ftruncate(shmd, sizeof(char));                                                                  // file named by path & sizeof length
+    string[SIZE] = (char *)mmap(NULL, sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmd, 0); // virtual address space of the calling process IT HASaddress.length,prot,flags,fd,offset.
+
+
+
+   
+    FILE *fp=fopen("dictionary.txt", "r");
+
+    if (fp==NULL)
+    {
+        printf("File opening Error!!");
+    }
+    while(fgets(string,SIZE,fp)!=NULL)
+         if(string[0]=='m'||string[0]=='M')
+         {
+             i++;
+             printf("%s %d",string,i);
+         }
     
-
-
-        
-            
     
-      printf("Enter the string starting letter: ");
-      scanf("%s",str);
-     str[1000] = *count;
-       
-
+    printf("\nsucessfull");
+    fclose(fp);
     return 0;
 }
